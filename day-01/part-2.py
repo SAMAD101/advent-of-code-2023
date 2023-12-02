488702ef18b1cf66130afc6bc1b24a71039e2859
Sam <asamadans@gmail.com>
2023-12-02 23:13:04 +0530

    modified part 2

--- day-01/part-2.py
+++ day-01/part-2.py
@@ -6,14 +6,25 @@ from typing import List
 
 def find_the_answer(inputs: List[str]) -> int:
     sum: int = 0
-    dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9", "zero": "0"}
+    NUMS_MAP = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
     for string in inputs:
-        matches = re.findall(r'(\d|one|two|three|four|five|six|seven|eight|nine|zero)', string)
-        if(matches[0] in dict):
-            matches[0] = dict[matches[0]]
-        if(matches[-1] in dict):
-            matches[-1] = dict[matches[-1]]
-        sum += int(matches[0] + matches[-1])
+        first = None
+        last = None
+        while(True):
+            match = re.search(r'\d|one|two|three|four|five|six|seven|eight|nine', string)
+            if(match):
+                if not first:
+                    first = match.group(0)
+                    string = string[match.end():]
+                    continue
+                last = match.group(0)
+                string = string[match.end():]
+            else:
+                break
+        if last is None: last = first
+        if first in NUMS_MAP.keys(): first = NUMS_MAP[first]
+        if last in NUMS_MAP.keys(): last = NUMS_MAP[last]
+        sum += int(first + last)
 
     return sum
 
