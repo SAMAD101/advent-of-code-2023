87e3b438eb9620a21d7a89efd1a24704d3f50a79
Sam <asamadans@gmail.com>
2023-12-02 15:53:58 +0530

    day 1 part 1

---
+++ day-01/part-1.py
@@ -0,0 +1,21 @@
+# Trebuchet?!
+
+import re
+
+
+def find_the_answer(input: list) -> int:
+    sum = 0
+    for string in input:
+        matches = re.findall(r'\d', string)
+        sum += int(matches[0] + matches[-1])
+
+    return sum
+
+if(__name__=='__main__'):
+    inputs = []
+    with open("inputs.txt", "r") as f:
+        for line in f:
+            inputs.append(line.strip())
+
+    print(find_the_answer(inputs))
+
